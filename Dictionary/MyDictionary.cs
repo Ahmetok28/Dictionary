c973a5adc397a29a3618eab9859b5704da6cbb08
Ahmet ÖK <77897859+Ahmetok28@users.noreply.github.com>
2023-01-23 20:43:56 +0300

    Proje dosyası ekle.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public class MyDictionary<T>
+    {
+        T[] items;
+        //T[] tempItem;
+        public MyDictionary() 
+        {
+            items= new T[0];
+
+        }
+        public void Add(T data)
+        {
+            T[] tempItem = items;
+            items=new T[items.Length+1];
+            for (int i = 0; i < tempItem.Length; i++)
+            {
+                items[i] = tempItem[i];
+            }
+            items[items.Length-1] = data;
+        }
+
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public T[] Items
+        {
+            get { return items; }
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> sehiler= new MyDictionary<string>();
+            sehiler.Add("Giresun"); 
+            sehiler.Add("Bursa"); 
+            sehiler.Add("İstanbul"); 
+            sehiler.Add("Ankara");
+            
+            sehiler.Add("Sakarya");
+            Console.WriteLine(sehiler.Length);
+            Console.WriteLine();
+
+            foreach (var item in sehiler.Items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
